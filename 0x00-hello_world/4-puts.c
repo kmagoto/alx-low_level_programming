b0664aff9824c48d1070f62be8d8af94a57afc8d
kmagoto <kennedymagoto@gmail.com>
2021-06-17 11:20:22 -0700

    prints text followed by new line using function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@ int main(void)
 /**
  *main - Entry point
  *Return: Always 0 (Success)
-*/ 
+*/
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
