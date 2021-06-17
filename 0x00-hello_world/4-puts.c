acfb3fe7d6a16f05ccd333e699cc791b6dce9df5
kmagoto <kennedymagoto@gmail.com>
2021-06-17 11:03:34 -0700

    prints text followed by new line using function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main()
+
+   /**
+puts - outputs the text enclosed
+   */
+{
+  puts("Programming is like building a multilingual puzzle"\n);
+  return(0);
+}
