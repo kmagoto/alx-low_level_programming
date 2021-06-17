5e4055666ab2c5b47f9a43b99dbad73c24765af4
kmagoto <kennedymagoto@gmail.com>
2021-06-17 11:26:28 -0700

    prints text followed by new line using function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main(void)
-
 /**
- *main - Entry point
+ *main -Entry point
  *Description: Prints the enclosed chars
  *Return: Always 0 (Success)
 */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
