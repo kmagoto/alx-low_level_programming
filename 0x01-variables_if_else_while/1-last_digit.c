f79ed00583fc8b85ed5c221ba2a5b37c48e0cc43
kmagoto <kennedymagoto@gmail.com>
2021-06-20 06:29:44 -0700

    prints a random number and tell if it's greater than 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/* more headers goes there */
 
-/**
- *main - Entry point
- *Return: Always 0 (Success)
+/* main = Entry point
+*Return: Always zero
 */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	printf("Last digit of %i is %i and is ", n, (n % 10));
-
-	if ((n % 10) == 0)
+	printf("Last digit of %i is %i\n", n, (n % 10));
+	if ((n % 10) > 5)
 	{
-		printf("0\n");
+	    printf("and is greater than 5\n");
 	}
-	else if ((n % 10) > 5)
+	else if ((n % 10) == 0)
 	{
-		printf("greater than 5\n");
+	    printf("and is 0\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0\n");
+	    printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
