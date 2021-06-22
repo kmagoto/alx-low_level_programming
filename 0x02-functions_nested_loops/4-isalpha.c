eabfc623fa191cdc6a9898289062717b8c68563e
kmagoto <kennedymagoto@gmail.com>
2021-06-22 11:08:37 -0700

    checks for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_isalpha - checks for alphabetic characters
+ *@c: character to be checked
+ *Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
