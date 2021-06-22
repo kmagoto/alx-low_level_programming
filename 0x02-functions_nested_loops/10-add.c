ccadff32c41f2d4fe8e7fa927ada300f9b9a4b8c
kmagoto <kennedymagoto@gmail.com>
2021-06-22 13:10:24 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *add - adds two integers and returns the result
+ *@a: first integer argument
+ *@b: second integer argument
+ *Return: sum
+ */
+
+int add(int a, int b)
+{
+	int add(int a, int b)
+	{
+		return (a + b);
+	}
