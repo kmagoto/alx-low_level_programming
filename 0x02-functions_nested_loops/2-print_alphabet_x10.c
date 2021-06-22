d49c31663ef80fab853175bd5f2da1e838bf657e
kmagoto <kennedymagoto@gmail.com>
2021-06-22 10:49:24 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 -print alphabet 10 times
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
