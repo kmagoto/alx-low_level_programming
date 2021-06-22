78e3eed75bcd194f42eb1c6c486237f99ffd0342
kmagoto <kennedymagoto@gmail.com>
2021-06-22 12:51:27 -0700

    prints every minute of the day of Jack Bauer from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,10 +15,10 @@ void jack_bauer(void)
 		while (min < 60)
 		{
 			_putchar('0' + (hours / 10));
-			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
 			_putchar(':');
 			_putchar('0' + (min / 10));
-			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
 			_putchar('\n');
 			min++;
 		}
