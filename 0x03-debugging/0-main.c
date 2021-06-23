1f2ad216c2472d0e18875cc7dd5c33667206e200
kmagoto <kennedymagoto@gmail.com>
2021-06-23 08:54:10 -0700

    test file for function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ *main - tests if the function prints positive or negative integer
+ *Return: Always 0
  */
 
 int main(void)
