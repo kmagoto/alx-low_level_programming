63e287fa4459e2480f77cb1b7f72c114dc67c1e2
kmagoto <kennedymagoto@gmail.com>
2021-06-24 11:20:30 -0700

    print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *more_numbers - prints 0-14 ten times
+ */
+
+
+void more_numbers(void)
+{
+	char i;
+	char count;
+
+	for (i = 0; i <= 14; i++)
+	{
+		for (count = 0; count <= 10; count++)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+}
