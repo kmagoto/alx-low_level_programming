f20d3c2f62f5180910a47207a35489aee6ddc39d
kmagoto <kennedymagoto@gmail.com>
2021-06-24 10:02:48 -0700

    print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,17 @@
 
 /**
  *print_numbers - prints numbers from 0-9
+ *@a: character to print
  *Return: 0
  */
 
 void print_numbers(void)
 {
-	int a = 0;
+	int a = '0';
 
-	while (a < 9)
+	while (a <= '9')
 	{
-		_putchar('a');
+		_putchar(a);
 		a++;
 	}
 
