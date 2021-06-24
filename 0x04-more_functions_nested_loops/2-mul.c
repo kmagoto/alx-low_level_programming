8c72f02e0d59ede02e6e8be3c0bbbfaffe46164b
kmagoto <kennedymagoto@gmail.com>
2021-06-24 09:48:19 -0700

    product of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: character one to multiply
+ *@b: character two to multiply
+ *Return: product of a, b
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
