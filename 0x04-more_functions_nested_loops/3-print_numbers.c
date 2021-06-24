87653435413724a91a14db0c933d7fffb9cf5087
kmagoto <kennedymagoto@gmail.com>
2021-06-24 09:56:10 -0700

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints numbers from 0-9
+ *Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	if (a >= '0' && a <= '9')
+	{
+		_putchar('a');
+	}
+
+	_putchar("\n");
+	return (0);
+}
