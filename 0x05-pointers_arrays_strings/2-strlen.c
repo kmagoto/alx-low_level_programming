b0dd721fc9059782866ff2fb2e11ed61c035c277
kmagoto <kennedymagoto@gmail.com>
2021-06-28 07:31:41 -0700

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns length of a string
+ *@s: the string to check length
+ *Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	char s;
+	int len;
+
+
+	len = strlen(s);
+	_putchar(s);
+
+	return (0);
+}
