ae899ca4d912089dea108de47099bb6fa0b494c7
kmagoto <kennedymagoto@gmail.com>
2021-06-28 08:10:33 -0700

    prints string normally, then reverses

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ *_strlen - shows length of string
+ *@s: string to reverse
+ *Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ *rev_string - function to reverse string
+ *@s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
+
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
+	}
+}
