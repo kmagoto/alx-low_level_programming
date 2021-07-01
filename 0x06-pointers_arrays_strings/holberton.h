ce8dbe05641ab49859dc04851bffc48e8dc99adf
kmagoto <kennedymagoto@gmail.com>
2021-07-01 06:31:51 -0700

    resources

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -13,7 +13,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-/*ski*/
+int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
