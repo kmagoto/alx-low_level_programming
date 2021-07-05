f67a934576bd518686c06bbc156f8e84e1f68103
kmagoto <kennedymagoto@gmail.com>
2021-07-05 06:48:59 -0700

    copies the memory area from one source to the destination

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *_memcpy - copies memory area
+ *@src: source
+ *@dest: destination
+ *@n: number of bytes
+ *Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int size  = n;
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			*(dest + i) = *(src + i); /*also dest[i] = src[i]*/
+		}
+	}
+	return (dest);
+}
