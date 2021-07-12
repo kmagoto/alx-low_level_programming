1eed930216f9daf2782a972bf07ed054113fc753
kmagoto <kennedymagoto@gmail.com>
2021-07-12 07:35:09 -0700

    prints all args passed to command line

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,14 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = 0;
 
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+
 	return (0);
 }
