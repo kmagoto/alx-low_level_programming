67ca5ffe056e64d7847890f9312aa8098d9aeae9
kmagoto <kennedymagoto@gmail.com>
2021-08-09 10:04:52 -0700

    reads a text file and prints it to POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,50 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints to POSIX stdout
- * @filename: name of the file that's read
- * @letters: the number of letters read and printed
- *
- * Return: the actual  number of letters read and printed, 0 if file cannot be
- * opened or read, 0 if @filename is NULL, 0 if write fails or doesn't write
- * the expected amount of bytes
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  char *buff;
-  int fd, rd, wr;
+	int fd; /* file descriptor */
+	ssize_t n_read, n_wrote;
+	char *buffer;
 
-  if (filename == NULL)
-    return (0);
-  buff = malloc(letters);
-  if (buff == NULL)
-    return (0);
+	if (filename == NULL)
+		return (0);
 
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-    {
-      free(buff);
-      return (0);
-    }
+	/* open */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-  rd = read(fd, buff, letters);
-  if (rd == -1)
-    {
-      free(buff);
-      close(fd);
-      return (0);
-    }
+	/* malloc buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-  wr = write(STDOUT_FILENO, buff, rd);
-  if(wr == -1)
-    {
-      free(buff);
-      close(fd);
-      return (0);
-    }
+	/* read */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-  close(fd);
-  free(buff);
-  return (wr);
+	/* write */
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (n_wrote == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (n_read);
+
 }
