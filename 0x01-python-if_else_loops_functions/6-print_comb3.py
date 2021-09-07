5ed49c4f8e922d56048ba0be285f6e60d89f49da
kmagoto <kennedymagoto@gmail.com>
2021-09-07 13:13:42 +0000

    refactor

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n in range(0, 10):
-    for j in range((i+1), 10):
-        if (i is not 8) or (j is not 9):
-            print("{}{}, ".format(i, j), end="")
+    for j in range((n+1), 10):
+        if (n is not 8) or (j is not 9):
+            print("{}{}, ".format(n, j), end="")
         else:
-            print("{}{}".format(i, j))
+            print("{}{}".format(n, j))
