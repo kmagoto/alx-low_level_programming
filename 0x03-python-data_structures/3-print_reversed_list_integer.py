60e4cbbc5f2224a66f39ebcf9c805f626af2ca97
kmagoto <kennedymagoto@gmail.com>
2021-09-13 09:03:49 +0000

    fix condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
         for i in my_list[::-1]: #fori in reversed(my_list)
-            print("{}".format(i))
+            print("{:d}".format(i))
