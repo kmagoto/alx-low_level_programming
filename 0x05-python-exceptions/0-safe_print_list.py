44c2562407d36729f702051ec25d6fa42896c241
kmagoto <kennedymagoto@gmail.com>
2021-09-27 07:37:39 -0700

    pycodestyle fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     idx = 0
     while idx < x:
@@ -10,4 +9,3 @@ def safe_print_list(my_list=[], x=0):
         idx += 1
     print("")
     return idx
-        
