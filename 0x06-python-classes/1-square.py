b857fe37adaf092b72aff545bbee167fc0c2280c
kmagoto <kennedymagoto@gmail.com>
2021-09-29 08:14:52 -0700

    pycodestyle fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,17 +5,18 @@ Defines class square with private attribute size
 
 """
 
+
 class Square:
     """
     class Square definition
-    
+
     Args:
         size : size of a side in square
     """
     def __init__(self, size):
         """
         Initializes square
-        
+
         Attributes:
             size : size of a side of square
         """
